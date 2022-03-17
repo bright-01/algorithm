e909e90da2c9202b64a20a9761ef26c54f27946b
mwchoen <mwcheon@cudo.co.kr>
2022-03-17 15:18:08 +0900

    005. 하샤드_수 풀이

---
+++ src/com/bright1/level1/하샤드_수_005.java
@@ -0,0 +1,71 @@
+package com.bright1.level1;
+
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 문제 설명
+ * 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+ *
+ * 제한 조건
+ * x는 1 이상, 10000 이하인 정수입니다.
+ * 입출력 예
+ * arr	return
+ * 10	true
+ * 12	true
+ * 11	false
+ * 13	false
+ *
+ * 입출력 예 설명
+ * 입출력 예 #1
+ * 10의 모든 자릿수의 합은 1입니다. 10은 1로 나누어 떨어지므로 10은 하샤드 수입니다.
+ *
+ * 입출력 예 #2
+ * 12의 모든 자릿수의 합은 3입니다. 12는 3으로 나누어 떨어지므로 12는 하샤드 수입니다.
+ *
+ * 입출력 예 #3
+ * 11의 모든 자릿수의 합은 2입니다. 11은 2로 나누어 떨어지지 않으므로 11는 하샤드 수가 아닙니다.
+ *
+ * 입출력 예 #4
+ * 13의 모든 자릿수의 합은 4입니다. 13은 4로 나누어 떨어지지 않으므로 13은 하샤드 수가 아닙니다.
+ *
+ * */
+public class 하샤드_수_005 {
+    public static void main(String[] args) {
+
+        int num = new Scanner(System.in).nextInt();
+        Solution_005 solution = new Solution_005();
+        System.out.println(solution.solution2(num));
+
+
+
+    }
+}
+
+class Solution_005 {
+    private int sum = 0;
+    public boolean solution(int x) {
+        int sum = 0;
+        int  num = x;
+        while(x >= 1) {
+            sum += x % 10;
+            x = x / 10;
+        }
+        return num % sum == 0;
+    }
+
+
+    // char - '0' 이유 -> '0'은 아스키코드 48.. '9' 는 아스키 코드 57 => 57 - 48 = 9
+
+    public boolean solution2(int x) {
+        Integer.toString(x).chars().forEach(i -> sum += i - '0');
+        return x % sum == 0;
+    }
+
+    public boolean solution3(int x) {
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
+    }
+}
+
+
