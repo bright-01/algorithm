437f5c23b800a89d7478224790f03ab85e739d92
mwchoen <mwcheon@cudo.co.kr>
2022-04-04 13:50:13 +0900

    시저_암호_017 풀이

--- src/com/bright1/Main.java
+++ src/com/bright1/Main.java
@@ -1,8 +1,30 @@
 package com.bright1;
 
+import com.bright1.programmers.MediaInfo;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MediaInfo mediaInfo = new MediaInfo();
+
+        mediaInfo.test1 = "test1";
+        mediaInfo.test2 = "test2";
+        mediaInfo.test3 = "test3";
+
+        setMediaTest(mediaInfo);
+        setMediaTest2(mediaInfo);
+
+        System.out.println(mediaInfo.toString());
     }
+
+    private static void setMediaTest(MediaInfo mediaInfo) {
+        mediaInfo.test4 = "test4";
+    }
+
+    private static void setMediaTest2(MediaInfo mediaInfo) {
+        mediaInfo.test5 = "test5";
+    }
+
+
 }
---
+++ src/com/bright1/programmers/MediaInfo.java
@@ -0,0 +1,23 @@
+package com.bright1.programmers;
+
+public class MediaInfo {
+    public String test1;
+    public String test2;
+    public String test3;
+    public String test4;
+    public String test5;
+    public String test6;
+
+
+    @Override
+    public String toString() {
+        return "MediaInfo{" +
+                "test1='" + test1 + '\'' +
+                ", test2='" + test2 + '\'' +
+                ", test3='" + test3 + '\'' +
+                ", test4='" + test4 + '\'' +
+                ", test5='" + test5 + '\'' +
+                ", test6='" + test6 + '\'' +
+                '}';
+    }
+}
