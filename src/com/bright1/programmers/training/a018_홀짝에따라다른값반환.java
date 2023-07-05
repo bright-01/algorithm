58f6d6fda28865a405449ea2e0a98d74dae2717a
finn <mwcheon@coolstay.co.kr>
2023-07-05 12:51:15 +0900

    홀짝에따라다른값반환

---
+++ src/com/bright1/programmers/training/a018_홀짝에따라다른값반환.java
@@ -0,0 +1,45 @@
+package com.bright1.programmers.training;
+
+/**
+ *
+ * 홀짝에 따라 다른 값 반환하기
+ * 문제 설명
+ * 양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+ *
+ * 제한사항
+ * 1 ≤ n ≤ 100
+ * 입출력 예
+ * n	result
+ * 7	16
+ * 10	220
+ * 입출력 예 설명
+ * 입출력 예 #1
+ *
+ * 예제 1번의 n은 7로 홀수입니다. 7 이하의 모든 양의 홀수는 1, 3, 5, 7이고 이들의 합인 1 + 3 + 5 + 7 = 16을 return 합니다.
+ * 입출력 예 #2
+ *
+ * 예제 2번의 n은 10으로 짝수입니다. 10 이하의 모든 양의 짝수는 2, 4, 6, 8, 10이고 이들의 제곱의 합인 22 + 42 + 62 + 82 + 102 = 4 + 16 + 36 + 64 + 100 = 220을 return 합니다.
+ *
+ * */
+public class a018_홀짝에따라다른값반환 {
+    public static void main(String[] args) {
+        int solution = solution(10);
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int n) {
+        int answer1 = 0;
+        int answer2 = 0;
+        int i = 1;
+
+        while (i <= n) {
+            if (i % 2 != 0) {
+                answer1 += i;
+            } else {
+                answer2 = answer2 + ( i * i );
+            }
+            i++;
+        }
+        return n % 2 != 0 ? answer1 : answer2;
+    }
+}
